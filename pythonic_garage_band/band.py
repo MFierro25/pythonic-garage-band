950ea9081f159efc7032f6c2f3ef121e7701f224
Miguel Fierro <emfierro2@gmail.com>
2021-12-03 08:17:18 -0800

    completed more tests and finished band class

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,39 +1,66 @@
-from collections import namedtuple
-
-
 class  Band():
-    def __init__(self, name, members):
+    instances = []
+    def __init__(self, name, members=[]):
         self.name = name
         self.members = members
-    
+         
+    def play_solos(self):
+       solos = []
+       for member in self.members:
+           solos.append(member.play_solo())
+       return solos
+   
 class Musician():
-    pass     
-
-class Guitarist:
+    def __str__(self):
+        return "My name is " + str(self.name) + " and I play " + str(self.instrument)      
+    def __repr__(self):
+        if self.instrument == 'guitar':
+            musician_type = 'Guitarist'
+        elif self.instrument == 'bass':
+            musician_type = 'Bassist'
+        elif self.instrument == 'drums':
+            musician_type = 'Drummer'
+        rep = f"{musician_type} instance. Name = " + self.name
+        return rep
     
-    def __init__(self, name):
+    def get_instrument(self):
+        return self.instrument
+    
+ 
+class Guitarist(Musician):
+    
+    def __init__(self, name='unkown'):
         self.name = name
         self.instrument = 'guitar'      
-    def __str__(self):
-        return 'My name is ' + str(self.name) + ' and I play ' + str(self.instrument) 
+    
     def __repr__(self):
         return 'Guitarist instance. Name = ' + str(self.name)
     
-class Drummer:
-    def __init__(self, name, ):
+    def play_solo(self):
+        return "face melting guitar solo"
+    
+    
+class Drummer(Musician):
+    def __init__(self, name='unkown' ):
         self.name = name
         self.instrument = 'drums'          
-    def __str__(self):
-        return 'My name is ' + str(self.name) + ' and I play ' + str(self.instrument)    
+        
     def __repr__(self):
         return 'Drummer instance. Name = ' + str(self.name)
+    
+    def play_solo(self):
+        return "rattle boom crash"
+    
 
-class Bassist:
-    def __init__(self, name, ):
+class Bassist(Musician):
+    def __init__(self, name='unkown' ):
         self.name = name
         self.instrument = 'bass'         
-    def __str__(self):
-        return 'My name is ' + str(self.name) + ' and I play ' + str(self.instrument)    
+    
     def __repr__(self):
-        return 'Bassist instance. Name = ' + str(self.name) 
+        return 'Bassist instance. Name = ' + str(self.name)
+    
+    def play_solo(self):
+        return "bom bom buh bom"
+     
     
