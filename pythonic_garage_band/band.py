17196c333d456c0610d64d313259ac29d1a0a269
Miguel Fierro <emfierro2@gmail.com>
2021-12-02 23:32:20 -0800

    have first 8 tests pass

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -2,42 +2,38 @@ from collections import namedtuple
 
 
 class  Band():
-    pass
+    def __init__(self, name, members):
+        self.name = name
+        self.members = members
     
 class Musician():
     pass     
 
 class Guitarist:
     
-    def __init__(self, name, ):
+    def __init__(self, name):
         self.name = name
-        self.instrument = 'guitar'
-          
+        self.instrument = 'guitar'      
     def __str__(self):
-        return 'My name is ' + str(self.name) + ' and I play ' + str(self.instrument)
-    
+        return 'My name is ' + str(self.name) + ' and I play ' + str(self.instrument) 
     def __repr__(self):
         return 'Guitarist instance. Name = ' + str(self.name)
     
 class Drummer:
     def __init__(self, name, ):
         self.name = name
-        self.instrument = 'drums'
-          
+        self.instrument = 'drums'          
     def __str__(self):
-        return 'My name is ' + str(self.name) + ' and I play ' + str(self.instrument)
-    
+        return 'My name is ' + str(self.name) + ' and I play ' + str(self.instrument)    
     def __repr__(self):
         return 'Drummer instance. Name = ' + str(self.name)
 
 class Bassist:
     def __init__(self, name, ):
         self.name = name
-        self.instrument = 'bass'
-          
+        self.instrument = 'bass'         
     def __str__(self):
-        return 'My name is ' + str(self.name) + ' and I play ' + str(self.instrument)
-    
+        return 'My name is ' + str(self.name) + ' and I play ' + str(self.instrument)    
     def __repr__(self):
         return 'Bassist instance. Name = ' + str(self.name) 
     
